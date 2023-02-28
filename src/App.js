96c2acd25ff9d5fd3b33816a77abfb190aa59540
Sajjad Hussain <121517125+sajjadhussain12345@users.noreply.github.com>
2023-03-01 00:11:59 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0); 
+  return (
+    <div className="container my-5 ">
+      <div className="card text-center my-5">
+        <div className="card-body">
+          <h1>Counter App</h1>
+          <div className="my-5">
+            <h2 className='my-5'>{count}</h2>
+            <button className="btn btn-success mx-3" onClick={() => setCount(count + 1)}>Increase</button>
+            <button className="btn btn-danger mx-3" onClick={() => setCount(count - 1)}>Decrease</button>
+            <button className="btn btn-secondary mx-3" onClick={() => setCount(0)}>Reset</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
